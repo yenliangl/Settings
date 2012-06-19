9f47144f950e35f50e7a4d17d95a66323185f432
Liu Yen-Liang (josh) <yenliangl@gmail.com>
2012-06-19 12:19:20 +0800

    Added Emacs-w3m configuration

--- Emacs/emacs.el
+++ Emacs/emacs.el
@@ -462,6 +462,28 @@
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
 ;; ----------------------------------------------------------------------
+;; Emacs/w3m
+;; ----------------------------------------------------------------------
+(add-to-list 'load-path (concat LISP_HOME "/emacs-w3m/lisp"))
+(require 'w3m-load)
+(setq browse-url-browser-function 'w3m-browse-url
+      browse-url-new-window-flag t
+      )
+(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+;; optional keyboard short-cut
+(global-set-key "\C-xm" 'browse-url-at-point)
+
+(setq w3m-default-display-inline-images t ;show image inline
+      w3m-use-cookies t
+      w3m-use-tab t                     ;C-cC-t to browse in a new tab
+      w3m-coding-system 'utf-8
+      w3m-file-coding-system 'utf-8
+      w3m-file-name-coding-system 'utf-8
+      w3m-input-coding-system 'utf-8
+      w3m-output-coding-system 'utf-8
+      w3m-terminal-coding-system 'utf-8)
+
+;; ----------------------------------------------------------------------
 ;; GUI config
 ;; ----------------------------------------------------------------------
 (when window-system
