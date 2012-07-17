564338e858ac42203337bc3dd0fd804da7f1f5f0
Liu Yen-Liang (josh) <yenliangl@gmail.com>
2012-07-17 13:54:39 +0800

    Do not list NEXT entry with REFILE tag

--- Emacs/org.el
+++ Emacs/org.el
@@ -453,7 +453,7 @@
                       ;(org-agenda-todo-ignore-deadlines t)))
                      ))
 
-          (tags-todo "+TODO=\"NEXT\""
+          (tags-todo "+TODO=\"NEXT\"-REFILE"
                      ((org-agenda-overriding-header "[NEXT Actions]")
                       ;(org-agenda-tags-todo-honor-ignore-options t)
                       ;(org-agenda-todo-ignore-scheduled t)
