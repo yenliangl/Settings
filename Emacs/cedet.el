ce6e7b02c25f4578dbafa030e33e3543e06cd66f
Liu Yen-Liang (josh) <yenliangl@gmail.com>
2014-07-12 14:14:01 +0800

    update emacs/jave development environment

--- Emacs/cedet.el
+++ Emacs/cedet.el
@@ -1,15 +1,69 @@
 ;; ----------------------------------------------------------------------
-;; CEDET for Emacs 23+
+;; CEDET for Emacs 24
 ;; ----------------------------------------------------------------------
 
 ;; suppress warning message of "cedet-called-interactively-p called with 0
 ;; arguments, but requires 1".
 (setq byte-compile-warning nil)
 
-(load-file (concat LISP_HOME "/cedet/trunk/cedet-devel-load.el"))
-;; Enable EDE (Project Management) features
-(global-ede-mode t)
+(setq CEDET_HOME (file-name-as-directory (concat LISP_HOME "/cedet/trunk/")))
+(load-file (concat CEDET_HOME "cedet-devel-load.el"))
+(add-to-list 'load-path (concat CEDET_HOME "contrib"))
 
-;; (semantic-load-enable-minimum-features)
-(semantic-load-enable-code-helpers)
+;; select which submodes we want to activate
+(add-to-list 'semantic-default-submodes 'global-semantic-mru-bookmark-mode)
+(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
+(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-highlight-func-mode)
+(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+
+;; Activate semantic
+(semantic-mode 1)
+
+;; load contrib library
+(require 'eassist)
+
+;; customisation of modes
+(defun alexott/cedet-hook ()
+  (local-set-key [(control return)] 'semantic-ia-complete-symbol-menu)
+  (local-set-key "\C-c?" 'semantic-ia-complete-symbol)
+  ;;
+  (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
+  (local-set-key "\C-c=" 'semantic-decoration-include-visit)
+
+  (local-set-key "\C-cj" 'semantic-ia-fast-jump)
+  (local-set-key "\C-cq" 'semantic-ia-show-doc)
+  (local-set-key "\C-cs" 'semantic-ia-show-summary)
+  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
+  )
+(add-hook 'c-mode-common-hook 'alexott/cedet-hook)
+(add-hook 'lisp-mode-hook 'alexott/cedet-hook)
+(add-hook 'scheme-mode-hook 'alexott/cedet-hook)
+(add-hook 'emacs-lisp-mode-hook 'alexott/cedet-hook)
+(add-hook 'erlang-mode-hook 'alexott/cedet-hook)
+
+(defun alexott/c-mode-cedet-hook ()
+  (local-set-key "\C-ct" 'eassist-switch-h-cpp)
+  (local-set-key "\C-xt" 'eassist-switch-h-cpp)
+  (local-set-key "\C-ce" 'eassist-list-methods)
+  (local-set-key "\C-c\C-r" 'semantic-symref)
+  )
+(add-hook 'c-mode-common-hook 'alexott/c-mode-cedet-hook)
+
+(semanticdb-enable-gnu-global-databases 'c-mode t)
+(semanticdb-enable-gnu-global-databases 'c++-mode t)
+
+(when (cedet-ectag-version-check t)
+  (semantic-load-enable-primary-ectags-support))
+
+;; SRecode
 (global-srecode-minor-mode 1)
+
+;; EDE
+(global-ede-mode 1)
+(ede-enable-generic-projects)
+
+;; Setup JAVA....
+(require 'cedet-java)
---
+++ Emacs/java.el
@@ -0,0 +1,46 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setting for Jave Programming Environment.
+;;
+
+;; Sets the basic indentation for Java source files
+;; to two spaces.
+
+(setq-default indent-tabs-mode nil)
+(defun fill-column-hook () (setq fill-column 80)) ; use 80 column width
+(defun comment-column-hook () (setq comment-column 40)) ; start comments in column 40
+
+;; cedet must be loaded before this file.
+(defun my-java-mode-hook ()
+  (font-lock-mode)
+
+  ;; --------------------------------------------------
+  ;; From kde-c++-mode-hook
+  ;; --------------------------------------------------
+  ;; (define-key java-mode-map "\C-m"  'c-context-line-break)
+  ;; (when (or (eq kde-tab-behavior 'default)
+  ;;           (eq kde-tab-behavior 'abbrev-indent))
+  ;;   (define-key java-mode-map "\C-i" 'agulbra-c++-tab))
+  ;; (define-key java-mode-map "\ef" 'c-forward-into-nomenclature)
+  ;; (define-key java-mode-map "\eb" 'c-backward-into-nomenclature)
+  ;; ;; Add (setq magic-keys-mode nil) to your .emacs (before loading this file)
+  ;; ;; to disable the magic keys in C++ mode.
+  ;; (and (boundp 'magic-keys-mode) magic-keys-mode
+  ;;      (progn
+  ;; ;;        ;; (define-key java-mode-map "\(" 'insert-parens)
+  ;; ;;        ;; (define-key java-mode-map "\)" 'insert-parens2)
+  ;;        (define-key java-mode-map "\," 'insert-comma)
+  ;; ;;        (define-key java-mode-map "\{" 'insert-curly-brace)
+  ;;        ))
+  (define-key java-mode-map "." 'semantic-complete-self-insert)
+
+  ;; --------------------------------------------------
+  ;; My customization
+  ;; --------------------------------------------------
+  (setq show-trailing-whitespace t)
+  ;; (setq c-basic-offset 4)
+  ;; (turn-off-auto-fill)
+  ;; (setq c-comment-start-regexp "\\(@\\|/\\(/\\|[*][*]?\\)\\)")
+  (modify-syntax-entry ?@ "< b" java-mode-syntax-table)
+  (local-set-key (quote [C-return]) (quote jde-complete))
+)
+(add-hook 'java-mode-hook 'my-java-mode-hook)
