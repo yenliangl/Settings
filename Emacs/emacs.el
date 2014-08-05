52a78e2a1b9215e6c63474f009f3c544e4d07cf1
Liu Yen-Liang (josh) <yenliangl@gmail.com>
2014-08-06 01:17:23 +0800

    remove unnecessary configuration for groovy mode

--- Emacs/emacs.el
+++ Emacs/emacs.el
@@ -449,14 +449,7 @@
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
 (add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
-
-(autoload 'run-groovy "inf-groovy" "Run an inferior Groovy process")
-(autoload 'inf-groovy-keys "inf-groovy" "Set local key defs for inf-groovy in groovy-mode")
-;;;
-(add-hook 'groovy-mode-hook
-          '(lambda ()
-             (inf-groovy-keys)
-             ))
+(add-to-list 'interpreter-mode-alist '("gradle" . groovy-mode))
 
 ;;; make Groovy mode electric by default.
 (add-hook 'groovy-mode-hook
