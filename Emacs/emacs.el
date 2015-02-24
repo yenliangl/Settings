bc12cd0ccbfe2e70a8302323623ffbbb91370ac9
Liu Yen-Liang (joe) <yenliangl@gmail.com>
2015-02-24 21:46:46 +0800

    add python-mode

--- Emacs/emacs.el
+++ Emacs/emacs.el
@@ -452,6 +452,13 @@
              (groovy-electric-mode)))
 
 ;; ----------------------------------------------------------------------
+;; Python mode
+;; ----------------------------------------------------------------------
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
+;; ----------------------------------------------------------------------
 ;; Twittering mode
 ;; ----------------------------------------------------------------------
 ;; (when window-system
